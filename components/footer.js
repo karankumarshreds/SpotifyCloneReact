d374c4c51b077564f9866c4743aed00117be2efb
Karan Kumar <nailedITnerd@gmail.com>
2020-08-07 18:25:48 +0530

    final commit

--- components/footer.js
+++ components/footer.js
@@ -4,25 +4,26 @@ import { useContext } from 'react';
 import { DataContext } from '../context/context-provider';
 // ICONS
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
 import RepeatIcon from '@material-ui/icons/Repeat';
 
 const Footer = () => {
-    const [{ item }] = useContext(DataContext)
+    const [{ song, playing }] = useContext(DataContext)
     return (
         <div className="footer">
             <div className="footer__left">
                 <img
                     className="footer__albumLogo"
-                    src={item?.album.images[0].url || "/art.jpg"}
-                    alt={item?.name}
+                    src={song?.album.images[0].url || "/art.jpg"}
+                    alt={song?.name}
                 />
-                {item ? (
+                {song ? (
                     <div className="footer__songInfo">
-                        <h4>{item.name || "Song name"}</h4>
-                        {/* <p>{item.artists.map((artist) => artist.name).join(", ")}</p> */}
+                        <h4>{song.name || "Song name"}</h4>
+                        <p>{song.artists.map((artist) => artist.name).join(", ")}</p>
                     </div>
                 ) : (
                         <div className="footer__songInfo">
@@ -34,7 +35,14 @@ const Footer = () => {
             <div className="footer__center">
                 <ShuffleIcon style={{ fontSize: 20 }} />
                 <SkipPreviousIcon style={{ fontSize: 20 }} />
-                <PlayCircleOutlineIcon className="footer__playButton" style={{ fontSize: 40 }} />
+                {
+                    playing === true
+                        ?
+                        <PauseCircleOutlineIcon className="songRow__active footer__playButton" style={{ fontSize: 40 }} />
+                        :
+                        <PlayCircleOutlineIcon className="footer__playButton" style={{ fontSize: 40 }} />
+
+                }
                 <SkipNextIcon style={{ fontSize: 20 }} />
                 <RepeatIcon style={{ fontSize: 20 }} />
             </div>
--- components/song-row.js
+++ components/song-row.js
@@ -9,7 +9,7 @@ import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 const SongRow = ({ track, changeSongState, song, playing }) => {
 
     let Icon = song?.id === track.id && playing
-        ? <PauseCircleFilledIcon />
+        ? <PauseCircleFilledIcon className={"songRow__active"} />
         : <PlayCircleFilledIcon />
 
     const playSong = async () => {
@@ -17,18 +17,24 @@ const SongRow = ({ track, changeSongState, song, playing }) => {
     };
     return (
         <div className="songRow" onClick={() => playSong()}>
-            <img className="songRow__album"
-                src={track.album.images[0]?.url}
-                alt="album-art" />
-            <div className="songRow__info">
-                <h1>{track.name}</h1>
-                <p>
-                    {track.artists.map(artist => artist.name).join(",")} -{" "}
-                    {track.album.name}
-                </p>
-            </div>
-            <div className="songRow__play">
-                {Icon}
+            <div className="songRow__inner">
+                <div className="songRow__left">
+                    <img className="songRow__album"
+                        src={track.album.images[0]?.url}
+                        alt="album-art" />
+                    <div className="songRow__info">
+                        <h1>{track.name}</h1>
+                        <p>
+                            {track.artists.map(artist => artist.name).join(",")} -{" "}
+                            {track.album.name}
+                        </p>
+                    </div>
+                </div>
+                <div className="songRow__right">
+                    <div className="songRow__play">
+                        {Icon}
+                    </div>
+                </div>
             </div>
         </div>
     )
